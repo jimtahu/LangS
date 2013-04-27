83aec26230a7ba54cc0e04a541c1ddc9d9bbde7b
jimtahu <jimtahu@gmail.com>
2013-04-27 13:32:11 -0500

    we now can assign and fetch variables

--- PrintStatement.cpp
+++ PrintStatement.cpp
@@ -5,12 +5,20 @@
  *      Author: jimtahu
  */
 
+#include <iostream>
+
 #include "PrintStatement.h"
 
 namespace ParseTree {
 
+using namespace std;
+
 PrintStatement::PrintStatement(Value *output) {
 	this->value=output;
+}
+
+void PrintStatement::Execute(){
+	cout<<this->value->getValue()<<endl;
 }
 
 PrintStatement::~PrintStatement() {
